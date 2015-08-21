60a6ac64ba62558b95e114c4375b35c6fd01d838
JakubT <jakub.tesarek@concur.com>
2015-08-21 21:39:13 +0200

    problem 3

---
+++ src/problem3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from itertools import count, islice
+
+def is_prime(n):
+	if n < 2: return False
+	return all(n % i for i in islice(count(start=2), int(n ** 0.5 - 1)))
+
+
+"""https://projecteuler.net/problem=3"""
+if __name__ == '__main__':
+	cmp = 600851475143
+	print(next(div for div in range(int(cmp ** 0.5), 1, -1) if cmp % div == 0 and is_prime(div)))
