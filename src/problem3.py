71cb30351e2af124e96b3c30643ff33bbd8503d6
JakubT <jakub.tesarek@concur.com>
2015-08-21 21:45:15 +0200

    optimize problem 3

--- src/problem3.py
+++ src/problem3.py
@@ -10,4 +10,4 @@ def is_prime(n):
 """https://projecteuler.net/problem=3"""
 if __name__ == '__main__':
 	cmp = 600851475143
-	print(next(div for div in range(int(cmp ** 0.5), 1, -1) if cmp % div == 0 and is_prime(div)))
+	print(next(div for div in xrange(int(cmp ** 0.5), 1, -1) if cmp % div == 0 and is_prime(div)))
