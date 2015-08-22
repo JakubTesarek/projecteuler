cf6ae8aa3c9c7f6b75b5a00f43c2ce9193ea28e6
JakubT <jakub.tesarek@concur.com>
2015-08-22 20:08:40 +0200

    problem 15

---
+++ src/problem_015.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+"""https://projecteuler.net/problem=16"""
+if __name__ == '__main__':
+	# it can also be calculated using simple formula (https://en.wikipedia.org/wiki/Lattice_path) but this is more fun
+	
+	grid = {}
+
+	for x in range(0, 21):
+		grid[x] = {}
+		for y in range(0, 21):
+			if x == y == 0:
+				grid[x][y] = 1
+			else:
+				x_value = grid[x - 1][y] if x - 1 in grid else 0
+				y_value = grid[x][y - 1] if y - 1 in grid[x] else 0
+				grid[x][y] = x_value + y_value
+
+	print(grid[20][20])
