b02464b50bf8a2357d28bceb127733655268c075
JakubT <jakub.tesarek@concur.com>
2015-08-22 14:09:23 +0200

    problem 12

---
+++ src/problem12.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+def factors(n):
+	factors = set([f for f in range(1, int(n ** 0.5) + 1) if n%f == 0])
+
+	for f in set(factors):
+		factors.add(n / f)
+
+	return factors
+
+"""https://projecteuler.net/problem="""
+if __name__ == '__main__':
+	total = 0
+	n = 0
+
+	while True:
+		n += 1
+		total += n
+
+		if len(factors(total)) > 500:
+			print total
+			break
