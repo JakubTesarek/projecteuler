678e8c79a631e2a9b77f95d51f1f7115c58dd450
JakubT <jakub.tesarek@concur.com>
2015-08-24 08:43:42 +0200

    problem 23

---
+++ src/problem_023.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from problem_021 import proper_factors
+
+"""https://projecteuler.net/problem=23"""
+if __name__ == '__main__':
+	abundant = []
+
+	for n in range(1, 28124):
+		if sum(proper_factors(n)) > n:
+			abundant.append(n)
+
+	numbers = set(range(1, 28124))
+
+	for k, n in enumerate(abundant):
+	 	for m in abundant[k:]:
+	 		numbers.discard(m + n)
+			
+	print(sum(numbers))
