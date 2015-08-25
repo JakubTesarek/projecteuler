04611be1063264d35add6e354241611925628e73
JakubT <jakub.tesarek@concur.com>
2015-08-25 10:22:15 +0200

    refactor prime generator to function

--- src/problem_010.py
+++ src/problem_010.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
-"""https://projecteuler.net/problem=10"""
-if __name__ == '__main__':
+def primes(max):
 	exclude = set()
-	primes = set()
+	primes = []
 
 	x = 2
-	while x < 2000000:
+	while x < max + 1:
 		if x not in exclude:
-			primes.add(x)
+			primes.append(x)
 			i = x
-			while i < 2000000:
+			while i < max + 1:
 				exclude.add(i)
 				i += x
 		x += 1
 
-	print(sum(primes))
+	return primes
+
+"""https://projecteuler.net/problem=10"""
+if __name__ == '__main__':
+	print(sum(get_primes(2000000)))
