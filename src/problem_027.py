438d0d536ab826f63a64aba8aa6c254012e795e6
JakubT <jakub.tesarek@concur.com>
2015-08-25 11:20:42 +0200

    problem 27

---
+++ src/problem_027.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from problem_010 import primes
+from problem_003 import is_prime
+
+"""https://projecteuler.net/problem="""
+if __name__ == '__main__':
+	
+	generator_product = None
+	max_primes_generated = None
+
+	for b in primes(1000):
+		for a in range(-999, 1000, 2):
+			n = 0
+			primes_generated = 0
+			while True:
+				product = (n ** 2) + (a * n) + b
+				if not is_prime(product):
+					break
+				
+				primes_generated += 1
+				n += 1
+			
+			if primes_generated > max_primes_generated:
+				max_primes_generated = primes_generated
+				generator_product = a * b
+
+	print(generator_product)
