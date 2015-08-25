5800f4ba3534d2e2cd1ff64a9b558a3bec392073
JakubT <jakub.tesarek@concur.com>
2015-08-25 13:38:12 +0200

    problem 32

---
+++ src/problem_032.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from problem_021 import proper_factors
+
+"""https://projecteuler.net/problem=32"""
+if __name__ == '__main__':
+	result = 0
+
+	for n in range(1, 10000):
+		for a in proper_factors(n):
+			if "".join(sorted(str(a) + str(n / a) + str(n))) == "123456789":
+				result += n
+				break
+
+	print(result)
