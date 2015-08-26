709ac248720833b28df7ae4c172674461e54c50b
JakubT <jakub.tesarek@concur.com>
2015-08-26 13:09:10 +0200

    problem 52

---
+++ src/problem_052.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+
+"""https://projecteuler.net/problem=52"""
+if __name__ == '__main__':
+
+	i = 0
+	while True:
+		i += 1
+		if sorted(str(i * 2)) == sorted(str(i * 3)) == sorted(str(i * 4)) == sorted(str(i * 5)) == sorted(str(i * 6)):
+			print(i)
+			break
+
+
+
